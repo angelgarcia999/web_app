cdb498998e765bade7c362dcb225321d0626f8c0
Angel Garcia <158326088+angelgarcia999@users.noreply.github.com>
2025-03-19 11:34:32 -0700

    Updated the output for verification of deployment

--- server.js
+++ server.js
@@ -4,8 +4,9 @@ const PORT = process.env.PORT || 80;
 
 app.get('/', (req, res) => {
     const obj = {
-        "message": "My name is Angel",
-        "timestamp": Date.now()
+        "message": "My name is Angel Garcia",
+        "timestamp": Date.now(),
+        "hello": "Hello World"
     };
     res.setHeader('Content-Type', 'application/json');
     res.send(JSON.stringify(obj, null, 2));
